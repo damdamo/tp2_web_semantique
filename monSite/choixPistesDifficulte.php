b6fe2b054e71f29131c175f6cfe9aa1d6033f5d0
Damien <damien.morard@etu.unige.ch>
2016-12-06 17:45:30 +0100

    Ajout des fichiers

---
+++ monSite/choixPistesDifficulte.php
@@ -0,0 +1,57 @@
+<?php
+	include_once('semsol/ARC2.php'); /* ARC2 static class inclusion */ 
+
+	$dbpconfig = array(
+	"remote_store_endpoint" => "http://ub414.duckdns.org:8080/rdf4j-workbench/repositories/tp2_damien/query",
+	);
+
+	$store = ARC2::getRemoteStore($dbpconfig); 
+
+	if ($errs = $store->getErrors()) {
+		echo "<h1>getRemoteSotre error<h1>" ;
+	}
+	
+		/*
+		{P:Comble :'.$_POST["niveau1"].' ?a}
+		UNION
+		{P:Comble :'.$_POST["niveau2"].' ?a}
+		*/
+
+	$query = '
+		PREFIX : <http://megeve.com/>
+		PREFIX P: <http://megeve.com/location/>
+		PREFIX R: <http://megeve.com/Restaurant/>
+		PREFIX H: <http://megeve.com/Hotels/>
+		PREFIX rdf:      <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+		PREFIX rdfs:     <http://www.w3.org/2000/01/rdf-schema#>
+		select distinct ?pointDepart ?pointArrive
+		where {  
+		  ?pointDepart :'.$_POST["choixAcces"].' ?pointArrive
+		}
+		limit 100
+	';
+
+
+	$rows = $store->query($query, 'rows'); /* execute the query */
+
+	if ($errs = $store->getErrors()) {
+		echo "Query errors" ;
+		print_r($errs);
+	}
+
+	/* display the results in an HTML table */
+	echo "<table border='1'>" ;
+	/* loop for each returned row */
+	foreach( $rows as $row ) { 
+		print "<tr>
+			<td>" .$row['pointDepart'] . "</td>
+		</tr>
+		<tr>
+			<td>" .$row['pointArrive'] . "</td>
+		</tr>";
+	}
+	echo "</table>"
+	
+?>
+
+<br/><input type="button" name="pagePrincipale" value="Revenir sur la page principale" onclick="self.location.href='siteWeb.php'"> 
---
+++ monSite/siteWeb.php
@@ -0,0 +1,54 @@
+﻿<!DOCTYPE html">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" >
+    <head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>Web semantique</title>
+		
+    </head>
+
+	
+    <body>
+		
+		<h1> TP2 WEB SEMANTIQUE <h1>
+		
+		<h3>Connaître tous les points possibles à partir d'un endroit et d'un type de piste donnée </h3>
+		<form method="post" action="pointsDisponible.php">
+		   <p>
+			   <label for="pointDepart">Point de depart</label><br />
+			   <select name="pointDepart" id="pointDepart">
+				   <option value="P:Ideal">Ideal</option>
+				   <option value="P:Comble">Comble</option>
+				   <option value="P:Mandarine">Mandarine</option>
+				   <option value="P:Sanglier">Sanglier</option>
+				   <option value="P:Brigants">Brigants</option>
+				   <option value="P:Vorasset">Vorasset</option>
+				   <option value="P:Etudiants">Etudiants</option>
+				   <option value="P:Grand-Bois">Grand-Bois</option>
+				   <option value="P:Ourson">Ourson</option>
+				   <option value="P:Mont-Joux">Mont-Joux</option>
+			   </select>
+			   
+			   <br/>
+			   <br/>
+			   
+			   <label for="choixAcces">Choix de connexion</label><br />
+			   <select name="choixAcces" id="choixAcces">
+				   <option value="lift">Remonté</option>
+				   <option value="greenRun">Piste verte</option>
+				   <option value="blueRun">Piste bleu</option>
+				   <option value="redRun">Piste rouge</option>
+				   <option value="blackRun">Piste noir</option>
+			   </select>
+			   
+			   <br/>
+			   <br/>
+			   
+			   <input type="submit" value="Envoyer" />
+		   </p>
+		</form>
+		
+		
+		<h3>Sélectionner les pistes d'un niveau de difficulté</h3>
+		
+    </body>
+</html>
